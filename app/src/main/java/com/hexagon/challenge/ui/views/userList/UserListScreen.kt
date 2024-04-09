0824b7a06a823c325a1f841bb6fc7349079cfdbc
jessmelo <jessmelo@usp.br>
2024-04-08 23:35:43 -0300

    removing unused imports

--- app/src/main/java/com/hexagon/challenge/ui/views/userList/UserListScreen.kt
+++ app/src/main/java/com/hexagon/challenge/ui/views/userList/UserListScreen.kt
@@ -14,11 +14,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.BitmapPainter
